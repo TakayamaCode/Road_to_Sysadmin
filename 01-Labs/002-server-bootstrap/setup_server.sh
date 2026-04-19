a0a760c9cab7c484b91386c6a35e206c702088f8
Brayan Barrera <nicolasbarrera05@hotmail.com>
2026-04-19 13:13:25 -0500

    refactor(Lab-002): eliminar secciones de despliegues no pertenecientes al Lab
    Detalles:
    - Remocion de bloques de instalacion de Nginx, Firewall y SSH.
    - Ajuste del alcance del script exclusivamente a auditoria inicial.
    - fixes #1

--- 01-Labs/002-server-bootstrap/setup_server.sh
+++ 01-Labs/002-server-bootstrap/setup_server.sh
@@ -105,56 +105,6 @@ sudo apt update
 sudo apt upgrade -y
 verificar
 
-
-# 4. Instalacion de Nginx
-
-echo "Instalando Nginx..."
-
-sudo apt install nginx -y
-verificar
-
-#==============================================================================
-# 5. Ahora configurare el firewall para que me permita abrir los puertos necesarios
-# para usar nginx. Usaré Nginx full, esto abrirá por defecto los siguientes 
-# puertos:
-#
-# Puerto 80: Para tráfico HTTP (sin cifrar).
-# Puerto 443: Para tráfico HTTPS (seguro/cifrado con SSL)
-#
-# NOTA: Me documentaré mejor por si hay buenas practicas para esto y haré una 
-# segunda version. dejo abierto 443 para un posible certificado SSL
-#==============================================================================
-
-echo "Configurando el firewall.."
-
-sudo ufw allow 'Nginx Full'
-verificar
-
-#==============================================================================
-# 6. Aquí me percate que si quiero aprender a conectarme por SSH tengo que abrir 
-# también el puerto 22, además si no lo configuro ahora, tendria que reiniciar
-# la maquina para poder volver a entrar , por que el firewall no me dejaria 
-# establecer una conexion
-#==============================================================================
-
-echo "Permitir conexion SSH con OpenSSH..."
-
-sudo ufw allow OpenSSH
-verificar
-
-# 7. Activar el firewall
-
-echo "Activando el firewall..."
-
-sudo ufw enable
-verificar
-
-# 8.Actualizando reglas
-
-echo "Actualizando reglas de firewall..."
-
-sudo ufw reload
-
 # 9. Verificacion de todo
 
 echo "Verificando instalaciones...."
